0fdd55931602c4d94699cad8ebdd10528630b124
yosida95 <kohei@yosida95.com>
2014-07-14 04:34:13 +0900

    Add WebUrl field to Project

--- projects.go
+++ projects.go
@@ -52,6 +52,7 @@ type Project struct {
 	Namespace            *Namespace `json:"namespace,omitempty"`
 	SshRepoUrl           string     `json:"ssh_url_to_repo"`
 	HttpRepoUrl          string     `json:"http_url_to_repo"`
+	WebUrl               string     `json:"web_url"`
 }
 
 /*
